614ee3520a77382243f3b57679bc6a8b6d6be3c7
Blacksite54 <ivanov.darkbee@inbox.ru>
2024-12-08 21:34:33 +0700

    feat: +swagger

--- api/main/urls.py
+++ api/main/urls.py
@@ -1,6 +1,21 @@
+from rest_framework.permissions import AllowAny
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1',
+        description="Test description",
+    ),
+    permission_classes=[AllowAny],
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,5 +27,6 @@ urlpatterns = [
         ),
         name="mails"
     ),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('', RedirectView.as_view(url='/email/ticket/', permanent=False)),
 ]
